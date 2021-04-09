904adeaf93e9df6220311803673f0bdf8b9e99b7
Lapwace <lopo74000@gmail.com>
2021-04-09 21:03:46 +0200

    Event detect

--- personnal_modules/create_canvas.js
+++ personnal_modules/create_canvas.js
@@ -4,3 +4,18 @@ const sdb = new SimpleDrawingBoard(document.getElementById('canvas'), {
     boardColor:   'transparent',
     historyDepth: 10
   });
+
+
+sdb.ev.on('drawBegin', () => {
+    sdb.getImg('./image/img.png')
+    console.log("starte")
+})
+
+sdb.ev.on('draw', () => {
+    console.log("C'est inc")
+})
+
+sdb.ev.on('drawEnd', () => {
+    sdb.setImg('./image/img.png', true)
+    console.log("end")
+})
